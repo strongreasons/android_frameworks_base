97cbc5e13c20222883ecb46cf3c8aa92c348b288
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    @ZeNiXxX - Bring to Android 10
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: strongreasons <abenkenari@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4536,6 +4536,16 @@ public final class Settings {
         public static final Validator SHOW_FOURG_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
