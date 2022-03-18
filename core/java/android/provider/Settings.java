c717baf0524a93fa9151a9e9012e484f8e6c5e3a
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    Ignore window secure flags [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9459,6 +9459,12 @@ public final class Settings {
         };
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
